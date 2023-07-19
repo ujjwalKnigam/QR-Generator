56b34193bdb0b90f73f9f3ecd72528a7724bec0f
ujjwalKnigam <ujjwalknigam1@gmail.com>
2023-07-19 12:52:15 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer"
+import qr from "qr-image"
+import fs from "fs"
+import { writeFile } from "fs";
+
+inquirer
+  .prompt([
+    {
+        message: "Enter a URL: ",
+        name: "URL"
+    }
+  ])
+  .then((answer) => {
+    let url = answer.URL
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('QR.png'));
+    writeFile("URL.txt",url,(err)=>{
+        if (err) throw err
+    console.log("The File has been saved!")
+    })
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
